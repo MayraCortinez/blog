b78ef09cd66aa12294ad48f5e91b9cf00941aa3c
MayC <mayrautopia@gmail.com>
2024-05-08 15:10:26 -0300

    add dark/light script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site : "https://cortinezblogastro.netlify.app/"
+  site: "https://cortinezblogastro.netlify.app/",
+  integrations: [preact()]
 });
